15892447177e3ff6913a8d61fd62a87a5a9610be
Ashwani <ashwanikumar150798@gmail.com>
2021-08-21 15:04:57 +0530

    Navbar completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import NavBar from './components/NavBar'
+import FriendForm from './components/FriendForm'
+
+export default function App() {
+  return (
+    <>
+      <NavBar />
+      <FriendForm />
+    </>
+  )
+}
