58f4d31e6c10c6cac4797ade4ad9768e5d29d758
Ashwani <ashwanikumar150798@gmail.com>
2021-08-21 23:02:22 +0530

    Routing completed, todo: work on form

--- backend/models/friend.js
+++ backend/models/friend.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose"
 
 const friendSchema = new mongoose.Schema({
   name: String,
-  age: Number
+  age: Number,
+  description: String
 })
 
 const Friend = mongoose.model('friend', friendSchema)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
 
 import NavBar from './components/NavBar'
+import HomePage from './pages/HomePage'
 import FriendForm from './components/FriendForm'
 
 export default function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-      <FriendForm />
-    </>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/add">
+          <FriendForm />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
