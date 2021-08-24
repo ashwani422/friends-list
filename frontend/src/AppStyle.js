ebccf851b7b609b4e4e230a2b6f34ffdcebbb0c6
Ashwani <ashwanikumar150798@gmail.com>
2021-08-24 21:58:01 +0530

    New file AppStyle.js

---
+++ frontend/src/AppStyle.js
@@ -0,0 +1,13 @@
+import createTheme from "@material-ui/core/styles/createTheme";
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffc107',
+      dark: '#ffab00'
+    }
+  }
+})
+
+export default theme
